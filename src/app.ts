761f00e268508081c589e8f619f0289409fbba13
Cristian Tala S <yomismo@cristiantala.cl>
2021-02-13 06:55:14 -0300

    Added Cache

--- src/app.ts
+++ src/app.ts
@@ -7,25 +7,44 @@ import {
 } from './models/Empresa';
 
 
+const memjs = require('memjs')
+const mc = memjs.Client.create(process.env.MEMCACHIER_SERVERS, {
+    failover: true, // default: false
+    timeout: 1, // default: 0.5 (seconds)
+    keepAlive: true // default: false
+})
+
+
 const puppeteer = new Puppeteer();
 const rutvalidator = require('chileanrutvalidator');
-const port:number|string = process.env.PORT || 3000;
+const port: number | string = process.env.PORT || 3000;
 
 app.get('/', async (req, res) => {
     res.send({
         message: "usa /byrut/:rut"
     });
-    
+
 });
 
 app.get('/byrut/:rut', async (req, res) => {
     const rutOriginal = req.params.rut;
     if (rutvalidator.validarRut(rutOriginal)) {
-        const DV = rutOriginal.slice(-1);
-        const RUT = rutOriginal.substring(0, rutOriginal.length - 1);
-        console.log(RUT, DV);
-        const empresa: Empresa = await puppeteer.scrap(RUT, DV);
-        res.send(empresa);
+        mc.get(rutOriginal, async (err: Error, cachedEmpresa: Empresa) => {
+
+            if (err == null && cachedEmpresa != null) {
+                res.send(cachedEmpresa);
+            } else {
+                const DV = rutOriginal.slice(-1);
+                const RUT = rutOriginal.substring(0, rutOriginal.length - 1);
+                console.log(RUT, DV);
+                const empresa: Empresa = await puppeteer.scrap(RUT, DV);
+                mc.set(rutOriginal, {expires:0}, (errSetCache:Error, resultSet:any) => {
+                    console.log(errSetCache,resultSet);
+                })
+                res.send(empresa);
+            }
+        })
+
     } else {
         res.status(500).send({
             message: "RUT INVALIDO"
